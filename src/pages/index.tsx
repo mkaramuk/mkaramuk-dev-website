0950a3b07bc3d4e22c69cc80e6cb9db8c8fe3514
Muhammed Karamuk <mkaramuk2002@gmail.com>
2023-08-22 19:52:23 +0300

    Main page updated + The technologies that used added. + A cool typewriter effect added. + Navbar updated for scrolling.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { motion } from "framer-motion";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { Icon } from "@iconify/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface NavbarLinkProps {
 	children: ReactNode;
@@ -10,7 +14,7 @@ interface NavbarLinkProps {
 
 function NavbarLink(props: NavbarLinkProps) {
 	return (
-		<motion.li
+		<motion.div
 			transition={{
 				duration: 1,
 				delay: props.delay,
@@ -23,34 +27,77 @@ function NavbarLink(props: NavbarLinkProps) {
 				opacity: 1,
 				y: 0,
 			}}
-			className={twMerge("text-white font-bold", "hover:cursor-pointer")}
+			className={twMerge(
+				"text-white font-bold rounded-full transition-all duration-300 p-4",
+				"hover:cursor-pointer hover:bg-gray-100 hover:text-black"
+			)}
 		>
 			<a href={props.href}>{props.children}</a>
-		</motion.li>
+		</motion.div>
+	);
+}
+
+function Tool(props: { icon: string }) {
+	return (
+		<div
+			className={twMerge(
+				"w-[calc(100%-20px)] h-[100px] rounded-full flex items-center justify-center p-2 bg-white"
+			)}
+		>
+			<Icon
+				className={twMerge(
+					"text-[80px]",
+					"sm:text-[60px]",
+					"md:text-[7 0px]"
+				)}
+				icon={props.icon}
+			/>
+		</div>
 	);
 }
 
 export default function Home() {
+	const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+	useEffect(() => {
+		function scroll(e: Event) {
+			setIsScrolled(window.scrollY > 0);
+		}
+
+		window.addEventListener("scroll", scroll);
+		return () => window.removeEventListener("scroll", scroll);
+	}, []);
+
 	return (
-		<main className="w-full h-screen bg-black ">
-			<ul
+		<main className="w-full h-screen">
+			<nav
 				className={twMerge(
-					"fixed p-3 flex w-full",
-					"justify-between px-[20px]",
-					"sm:justify-center sm:px-0 sm:gap-[100px]"
+					"fixed flex items-center justify-center w-full z-10"
 				)}
 			>
-				<NavbarLink delay={1} href="/projects">
-					Projects
-				</NavbarLink>
-				<NavbarLink delay={1.5} href="/about">
-					About
-				</NavbarLink>
-				<NavbarLink delay={2} href="/blog">
-					Blog
-				</NavbarLink>
-			</ul>
-			<div className="w-full h-full px-4">
+				<div
+					className={twMerge(
+						"transition-all duration-200 flex p-3 w-full justify-between",
+						`${
+							isScrolled
+								? "bg-gray-800 sm:rounded-lg shadow-2xl"
+								: "bg-none shadow-none rounded-none"
+						}`,
+						"sm:justify-center sm:gap-[100px] sm:w-fit sm:mt-4"
+					)}
+				>
+					<NavbarLink delay={1} href="/projects">
+						Projects
+					</NavbarLink>
+					<NavbarLink delay={1.5} href="/about">
+						About
+					</NavbarLink>
+					<NavbarLink delay={2} href="/blog">
+						Blog
+					</NavbarLink>
+				</div>
+			</nav>
+			<section className="w-full h-full flex flex-col px-4 bg-[linear-gradient(180deg,_rgba(0,0,0,1)_0%,_rgba(46,46,46,1)_100%)] snap-start snap-normal">
 				<div className="w-full flex justify-center pt-[100px]">
 					<motion.img
 						initial={{
@@ -85,23 +132,84 @@ export default function Home() {
 				>
 					<div
 						className={twMerge(
-							"text-white text-[25px] font-bold font-mono",
+							"text-white text-[21px] font-bold font-mono",
 							"sm:text-[30px]"
 						)}
 					>
 						Hi, I'm Muhammed
 					</div>
+				</motion.div>
+				<div className="w-full flex justify-center">
 					<div
 						className={twMerge(
-							"text-white font-mono",
-							"text-[13px]",
+							"flex items-center justify-center text-white font-bold font-mono",
+							"text-[8px]",
 							"sm:text-[15px]"
 						)}
 					>
-						A passionate devops and full stack developer
+						<div className="typewriter">
+							A passionate devops and full stack developer
+						</div>
 					</div>
-				</motion.div>
-			</div>
+				</div>
+
+				<div className="w-full h-full flex items-center justify-center">
+					<div className="justify-center flex flex-wrap w-full gap-2">
+						<div
+							className={twMerge(
+								"text-center text-white font-mono text-[13px]",
+								"sm:text-[15px]",
+								"md:text-[18px] md:mt-[10px]"
+							)}
+						>
+							The technologies that I have experienced
+						</div>
+						<div className="w-[99vw]">
+							<Slider
+								dots={false}
+								infinite
+								slidesToScroll={1}
+								slidesToShow={5}
+								autoplay
+								speed={5000}
+								autoplaySpeed={0}
+								cssEase="linear"
+								arrows={false}
+								responsive={[
+									{
+										breakpoint: 640,
+										settings: {
+											slidesToShow: 1,
+										},
+									},
+									{
+										breakpoint: 768,
+										settings: {
+											slidesToShow: 3,
+										},
+									},
+								]}
+							>
+								{[
+									"logos:docker-icon",
+									"logos:kubernetes",
+									"vscode-icons:file-type-cpp3",
+									"logos:nodejs",
+									"logos:terraform-icon",
+									"logos:rust",
+									"logos:python",
+									"logos:ansible",
+									"logos:typescript-icon",
+									"flat-color-icons:linux",
+								].map((x, i) => (
+									<Tool key={i} icon={x} />
+								))}
+							</Slider>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="w-full h-full px-4 bg-[linear-gradient(0deg,_rgba(0,0,0,1)_0%,_rgba(46,46,46,1)_100%)]  snap-start snap-normal"></section>
 		</main>
 	);
 }
